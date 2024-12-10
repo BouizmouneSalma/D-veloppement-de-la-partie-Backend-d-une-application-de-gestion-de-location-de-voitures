6d20381f423156c5a3383f08eaec562357e23f2e
BouizmouneSalma <salmabouizmoun@gmail.com>
2024-12-10 12:35:51 +0100

    "Added initial insert.php file with basic POST handling and database query."

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nom = $conn->real_escape_string($_POST['nom']);
+    $adresse = $conn->real_escape_string($_POST['adresse']);
+    $tel = $conn->real_escape_string($_POST['tel']);
+
+    $sql = "INSERT INTO client (nom, adresse, tel) VALUES ('$nom', '$adresse', '$tel')";
+
+}
+?>
